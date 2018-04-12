6b4978b63b4e55fb94c30118c570955761e16e63
João Otávio Mota Roriz <joaororiz@.github.com>
2018-04-12 20:08:19 -0300

    Primeiro commit

---
+++ 01-Integracao de aplicacao/teste/src/teste/Teste.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package teste;
+
+/**
+ *
+ * @author João Otávio Mota Roriz
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.println("teste.Teste.main()");
+    }
+
+}
