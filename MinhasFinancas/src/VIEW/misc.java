7bb7403c16049b8e6e63ec8755f9c9d7c987a795
bruno <bruno.coaracy@gmail.com>
2018-05-15 11:48:15 -0300

    Criada a classe misc no pacote VIEW, Não sei se está correto, mas pensei em utilizar essa classe para realizar cálculos e etc.

---
+++ MinhasFinancas/src/VIEW/misc.java
@@ -0,0 +1,17 @@
+package VIEW;
+
+import java.time.LocalDate;
+
+
+public class misc {
+    
+    
+    public LocalDate getDate(){ //Retorna a data atual
+        
+        LocalDate dataAtual = LocalDate.now();
+        return dataAtual;
+        
+    }
+           
+    
+}
