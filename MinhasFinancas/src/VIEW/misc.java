54e85422046e45367b82ff4ae9e169ecc8f2b03d
bruno.coaracy <bruno.coaracy@DSV-BRUNOCOARACY.nectarnet.com.br>
2018-06-05 17:51:20 -0300

    Adicionado método para informar pagamentos perto do vencimento. OBS: Fase de teste.

--- MinhasFinancas/src/VIEW/misc.java
+++ MinhasFinancas/src/VIEW/misc.java
@@ -1,6 +1,12 @@
 package VIEW;
 
+import CONTROLLER.Controller;
+import MODEL.DespesasBEAN;
+import MODEL.UsuarioBEAN;
+import java.sql.Date;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class misc {
 
@@ -8,6 +14,30 @@ public class misc {
         //Retorna a data atual
         LocalDate dataAtual = LocalDate.now();
         return dataAtual;
+    }
+    
+    public void avisarDespesasVencimento(UsuarioBEAN usuario){
+        ArrayList<DespesasBEAN> listaDespesas = Controller.listarDespesasApagar(usuario.getId());
+        int dataAtual = getDate().getDayOfMonth();
+        
+        if (listaDespesas != null) {
+            for (DespesasBEAN lista: listaDespesas) {
+                if(dataProxima(lista.getVencimento()))
+                    JOptionPane.showMessageDialog(null, "Pagamento a vencer:/n" + lista.getDescricao());
+            }
+        }
+    }
+    
+    private boolean dataProxima(String data){
+        
+        int dia = Integer.parseInt(data.substring(7));
+        int mes = Integer.parseInt(data.substring(5, 7));
+        int diaAtual = getDate().getDayOfYear();
+        int diaVencimento = (mes-2)*30+(mes/2)+28+dia;
+        if((diaVencimento>=diaAtual)&&(diaVencimento<=diaAtual+10))
+            return true;
+        return false;
+        
     }
 
 }
